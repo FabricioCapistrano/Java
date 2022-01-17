d4d13adc029ed8acfbea11bf3631897de6aa08db
Fabricio Capistrano <90995485+FabricioCapistrano@users.noreply.github.com>
2022-01-17 14:32:32 -0300

    Create Bondinho.java

---
+++ estudos/a1/src/Bondinho.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+
+public class Bondinho {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int Alunos = teclado.nextInt();
+        int Monitores = teclado.nextInt();
+        boolean quantidade = 50 >= Alunos+Monitores ;
+        if (quantidade == true)
+        {
+            System.out.println("S");
+        }
+        else {
+            System.out.println("N");
+        }
+        teclado.close();
+    }
+}
