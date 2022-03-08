45a03e4daba02bcc88f876f0c2c3ccd549ab5a22
Fabricio Capistrano <90995485+FabricioCapistrano@users.noreply.github.com>
2022-03-07 23:23:15 -0300

    aa

--- estudos/a1/src/Drone.java
+++ estudos/a1/src/Drone.java
@@ -5,11 +5,13 @@ import java.util.Scanner;
 public class Drone {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
+        
         int A = teclado.nextInt();
         int B = teclado.nextInt();
         int C = teclado.nextInt();
         int H = teclado.nextInt();
         int L = teclado.nextInt();
+
         int AB = A * B;
         int BC = B * C;
         int AC = A * C;
